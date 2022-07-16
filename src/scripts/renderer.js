21b5bd18abca9d3f9e4dc421abaeb8747d4f027a
augustinavicius <augustinavicius.ignas@gmail.com>
2022-07-16 06:59:15 +0300

    Map view fixes

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,7 +1,7 @@
 // IPC Renderer
 const { ipcRenderer } = require('electron');
 // Leaflet
-const L = require('leaflet');
+const leaflet = require('leaflet');
 
 // Update Status Event 
 // Changes the text on the footer div to show the current app update progress.
@@ -20,8 +20,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
     map.setView([47.70, 13.35], 7);
 
     // Initialize the base layer
-    var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // OpenStreet View
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    // Google Streets
+    leaflet.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    // Google Terrain
+    leaflet.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    // Google Hybrid
+    leaflet.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    // Google Sattelite
+    leaflet.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
 });
