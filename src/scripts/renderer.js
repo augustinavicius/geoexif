bdf1d7a8e82297404f94238ef55d84a276988cbb
augustinavicius <augustinavicius.ignas@gmail.com>
2022-07-16 06:34:50 +0300

    Added view columns & fixed footer div

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,8 +1,27 @@
 // IPC Renderer
 const { ipcRenderer } = require('electron');
+// Leaflet
+const L = require('leaflet');
 
 // Update Status Event 
 // Changes the text on the footer div to show the current app update progress.
 ipcRenderer.on('updatestatus', (event, text) => {
     document.getElementById('footer').innerHTML = text;
+});
+
+// Map Initialization
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Initialize the map
+    var map = L.map('map', {
+        scrollWheelZoom: false
+    });
+
+    // Set the position and zoom level of the map
+    map.setView([47.70, 13.35], 7);
+
+    // Initialize the base layer
+    var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 });
